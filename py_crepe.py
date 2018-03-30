6d9f0187b0f8fd2654ccad5507e57f9b293143a6
WojciechKusa <wojciech.kusa+github@gmail.com>
2018-03-30 15:40:53 +0200

    update crepe model to keras 2 API.

--- py_crepe.py
+++ py_crepe.py
@@ -1,36 +1,35 @@
 from keras.models import Model
 from keras.optimizers import SGD
 from keras.layers import Input, Dense, Dropout, Flatten
-from keras.layers.convolutional import Convolution1D, MaxPooling1D
+from keras.layers.convolutional import Conv1D, MaxPooling1D
 
-
-def model(filter_kernels, dense_outputs, maxlen, vocab_size, nb_filter,
+def model(filter_kernels, dense_outputs, maxlen, vocab_size, filters,
           cat_output):
     #Define what the input shape looks like
     inputs = Input(shape=(maxlen, vocab_size), name='input', dtype='float32')
 
     #All the convolutional layers...
-    conv = Convolution1D(nb_filter=nb_filter, filter_length=filter_kernels[0],
-                         border_mode='valid', activation='relu',
+    conv = Conv1D(filters=filters, kernel_size=filter_kernels[0],
+                         padding='valid', activation='relu',
                          input_shape=(maxlen, vocab_size))(inputs)
-    conv = MaxPooling1D(pool_length=3)(conv)
+    conv = MaxPooling1D(pool_size=3)(conv)
 
-    conv1 = Convolution1D(nb_filter=nb_filter, filter_length=filter_kernels[1],
-                          border_mode='valid', activation='relu')(conv)
-    conv1 = MaxPooling1D(pool_length=3)(conv1)
+    conv1 = Conv1D(filters=filters, kernel_size=filter_kernels[1],
+                          padding='valid', activation='relu')(conv)
+    conv1 = MaxPooling1D(pool_size=3)(conv1)
 
-    conv2 = Convolution1D(nb_filter=nb_filter, filter_length=filter_kernels[2],
-                          border_mode='valid', activation='relu')(conv1)
+    conv2 = Conv1D(filters=filters, kernel_size=filter_kernels[2],
+                          padding='valid', activation='relu')(conv1)
 
-    conv3 = Convolution1D(nb_filter=nb_filter, filter_length=filter_kernels[3],
-                          border_mode='valid', activation='relu')(conv2)
+    conv3 = Conv1D(filters=filters, kernel_size=filter_kernels[3],
+                          padding='valid', activation='relu')(conv2)
 
-    conv4 = Convolution1D(nb_filter=nb_filter, filter_length=filter_kernels[4],
-                          border_mode='valid', activation='relu')(conv3)
+    conv4 = Conv1D(filters=filters, kernel_size=filter_kernels[4],
+                          padding='valid', activation='relu')(conv3)
 
-    conv5 = Convolution1D(nb_filter=nb_filter, filter_length=filter_kernels[5],
-                          border_mode='valid', activation='relu')(conv4)
-    conv5 = MaxPooling1D(pool_length=3)(conv5)
+    conv5 = Conv1D(filters=filters, kernel_size=filter_kernels[5],
+                          padding='valid', activation='relu')(conv4)
+    conv5 = MaxPooling1D(pool_size=3)(conv5)
     conv5 = Flatten()(conv5)
 
     #Two dense layers with dropout of .5
@@ -40,7 +39,7 @@ def model(filter_kernels, dense_outputs, maxlen, vocab_size, nb_filter,
     #Output dense layer with softmax activation
     pred = Dense(cat_output, activation='softmax', name='output')(z)
 
-    model = Model(input=inputs, output=pred)
+    model = Model(inputs=inputs, outputs=pred)
 
     sgd = SGD(lr=0.01, momentum=0.9)
     model.compile(loss='categorical_crossentropy', optimizer=sgd,
